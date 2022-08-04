6c5b8fe7786af303da8d0d5b26168f130648588a
BmmNdr <abiemmibi@gmail.com>
2022-08-04 12:39:08 +0200

    added round function

--- app.js
+++ app.js
@@ -1,23 +1,32 @@
+//Button for player choice
 const ROCK = document.querySelector("#rock")
 const PAPER = document.querySelector("#paper")
 const SCISSORS = document.querySelector("#scissors")
 
+//identify player choice
 ROCK.addEventListener("click", function() {round("rock")})
 PAPER.addEventListener("click", function() {round("paper")})
 SCISSORS.addEventListener("click", function() {round("scissors")})
 
+
 //return a random choice between Rock, Paper or Scissors
+const CHOICES = ["rock", "paper", "scissors"]
 function getComputerChoice() {
-    let n = Math.floor(Math.random() * 3)
-    
-    if (n == 0)
-        return "Rock"
-    else if (n == 1)
-        return "Paper"
-    else
-        return "Scissors"
+    return CHOICES[Math.floor(Math.random() * 3)]
 }
 
-function round(choice) {
-    console.log(choice)
+function round(Pchoice) {
+    Cchoice = getComputerChoice();
+    console.log('Computer: ' + Cchoice)
+    console.log('Player: ' + Pchoice)
+
+    if(Pchoice == Cchoice)
+        return printResult("tie")
+        
+    printResult(((Cchoice == "rock" && Pchoice == "paper") || (Cchoice == "paper" && Pchoice == "scissors") || (Cchoice == "scissors" && Pchoice == "rock")) ? "Win" : "Lose")
+}
+
+function printResult(result)
+{
+    console.log(result)
 }
